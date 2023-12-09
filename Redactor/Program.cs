429bb4db1d81f8747d07da44a71a4c49f124972b
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2023-12-09 09:41:20 +0300

    Update Program.cs

--- Redactor/Program.cs
+++ Redactor/Program.cs
@@ -1,156 +1,53 @@
-using System;
-using System.IO;
-using System.Xml.Serialization;
 using Newtonsoft.Json;
+using System.Xml.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
-public class FileManager
-{
-    private string filePath;
-
-    public FileManager(string path)
-    {
-        filePath = path;
-    }
-
-    public Figure[] Load()
-    {
-        string extension = Path.GetExtension(filePath);
-
-        if (extension == ".txt")
-        {
-            return LoadFromTxt();
-        }
-        else if (extension == ".json")
-        {
-            return LoadFromJson();
-        }
-        else if (extension == ".xml")
-        {
-            return LoadFromXml();
-        }
-
-        throw new NotSupportedException("Unsupported file format");
-    }
-
-    private Figure[] LoadFromTxt()
-    {
-        string[] lines = File.ReadAllLines(filePath);
-        Figure[] figures = new Figure[lines.Length / 3];
-
-        for (int i = 0, j = 0; i < lines.Length; i += 3, j++)
-        {
-            string name = lines[i];
-            double width = Convert.ToDouble(lines[i + 1]);
-            double height = Convert.ToDouble(lines[i + 2]);
-
-            figures[j] = new Figure(name, width, height);
-        }
-
-        return figures;
-    }
-
-    private Figure[] LoadFromJson()
-    {
-        string json = File.ReadAllText(filePath);
-        return JsonConvert.DeserializeObject<Figure[]>(json);
-    }
-
-    private Figure[] LoadFromXml()
-    {
-        XmlSerializer serializer = new XmlSerializer(typeof(Figure[]));
-
-        using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
-        {
-            return (Figure[])serializer.Deserialize(fileStream);
-        }
-    }
-
-    public void Save(Figure[] figures)
-    {
-        string extension = Path.GetExtension(filePath);
-
-        if (extension == ".txt")
-        {
-            SaveAsTxt(figures);
-        }
-        else if (extension == ".json")
-        {
-            SaveAsJson(figures);
-        }
-        else if (extension == ".xml")
-        {
-            SaveAsXml(figures);
-        }
-        else
-        {
-            throw new NotSupportedException("Unsupported file format");
-        }
-    }
-
-    private void SaveAsTxt(Figure[] figures)
-    {
-        using (StreamWriter writer = new StreamWriter(filePath))
-        {
-            foreach (var figure in figures)
-            {
-                writer.WriteLine(figure.Name);
-                writer.WriteLine(figure.Width);
-                writer.WriteLine(figure.Height);
-            }
-        }
-    }
-
-    private void SaveAsJson(Figure[] figures)
-    {
-        string json = JsonConvert.SerializeObject(figures, Formatting.Indented);
-        File.WriteAllText(filePath, json);
-    }
-
-    private void SaveAsXml(Figure[] figures)
-    {
-        XmlSerializer serializer = new XmlSerializer(typeof(Figure[]));
-
-        using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
-        {
-            serializer.Serialize(fileStream, figures);
-        }
-    }
-}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Введите путь к файлу:");
+        Console.WriteLine("Введите путь к файлу, который вы хотите открыть");
         string filePath = Console.ReadLine();
 
-        FileManager fileManager = new FileManager(filePath);
-        Figure[] figures = fileManager.Load();
-
-        Console.WriteLine("Для сохранения файла нажмите F1. Для выхода - Escape.");
+        TextEditor textEditor = new TextEditor(filePath);
 
         while (true)
         {
-            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Сохранить файл в одном из трех форматов (txt, json, xml) - F1. Закрыть программу - Escape");
+            textEditor.text();
+            ConsoleKeyInfo key = Console.ReadKey();
 
-            if (keyInfo.Key == ConsoleKey.F1)
+            if (key.Key == ConsoleKey.F1)
             {
-                fileManager.Save(figures);
-                Console.WriteLine("Файл успешно сохранен.");
+                FileType fileType = textEditor.ChooseFileType();
+                if (fileType != FileType.Unknown)
+                {
+                    textEditor.Save(fileType);
+                    Console.WriteLine("Файл успешно сохранен на вашем рабочем столе.");
+                    Console.ReadKey();
+                }
             }
-            else if (keyInfo.Key == ConsoleKey.Escape)
+            else if (key.Key == ConsoleKey.Escape)
             {
                 break;
             }
         }
     }
 }
+
+[Serializable]
 public class Figure
 {
     public string Name { get; set; }
     public double Width { get; set; }
     public double Height { get; set; }
 
-    public Figure() { }
+    public Figure()
+    {
+    }
 
     public Figure(string name, double width, double height)
     {
@@ -158,4 +55,146 @@ public class Figure
         Width = width;
         Height = height;
     }
+}
+
+public class TextEditor
+{
+    private string filePath;
+    private List<Figure> figures;
+    public TextEditor(string filePath)
+    {
+        this.filePath = filePath;
+        this.figures = new List<Figure>();
+
+        if (File.Exists(filePath))
+        {
+            Load();
+        }
+        else
+        {
+            Console.WriteLine("Файл не существует. Создание новой модели.");
+        }
+    }
+
+    private void LoadFromTxtFile(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+
+        for (int i = 0; i < lines.Length; i += 3)
+        {
+            string name = lines[i];
+            double width = Convert.ToDouble(lines[i + 1]);
+            double height = Convert.ToDouble(lines[i + 2]);
+            figures.Add(new Figure(name, width, height));
+        }
+    }
+    public FileType ChooseFileType()
+    {
+        Console.WriteLine("Выберите тип файла для сохранения (1 - txt, 2 - json, 3 - xml):");
+        string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            return FileType.Txt;
+        }
+        else if (choice == "2")
+        {
+            return FileType.Json;
+        }
+        else if (choice == "3")
+        {
+            return FileType.Xml;
+        }
+        else
+        {
+            Console.WriteLine("Неверный выбор. Файл не будет сохранен.");
+            return FileType.Unknown;
+        }
+    }
+
+    public void Save(FileType fileType)
+    {
+        Console.WriteLine("Введите название файла для сохранения (без расширения):");
+        string fileName = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(fileName))
+        {
+            Console.WriteLine("Название файла не может быть пустым. Файл не будет сохранен.");
+            return;
+        }
+        string fileExtension = "." + fileType.ToString().ToLower();
+        string savePath = Path.Combine(Path.GetDirectoryName(filePath), fileName + fileExtension);
+
+        if (fileType == FileType.Txt)
+        {
+            SaveToTxtFile(savePath);
+        }
+        else if (fileType == FileType.Json)
+        {
+            string json = JsonConvert.SerializeObject(figures);
+            File.WriteAllText(savePath, json);
+        }
+        else if (fileType == FileType.Xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Figure>));
+            using (FileStream fileStream = new FileStream(savePath, FileMode.Create))
+            {
+                serializer.Serialize(fileStream, figures);
+            }
+        }
+    }
+    private void SaveToTxtFile(string filePath)
+    {
+        List<string> lines = new List<string>();
+        foreach (var figure in figures)
+        {
+            lines.Add(figure.Name);
+            lines.Add(figure.Width.ToString());
+            lines.Add(figure.Height.ToString());
+        }
+        File.WriteAllLines(filePath, lines);
+    }
+
+    public void Load()
+    {
+        string fileExtension = Path.GetExtension(filePath).ToLower();
+        if (fileExtension == ".txt")
+        {
+            LoadFromTxtFile(filePath);
+        }
+        else if (fileExtension == ".json")
+        {
+            string json = File.ReadAllText(filePath);
+            figures = JsonConvert.DeserializeObject<List<Figure>>(json);
+        }
+        else if (fileExtension == ".xml")
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Figure>));
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                figures = (List<Figure>)serializer.Deserialize(fileStream);
+            }
+        }
+    }
+
+
+    public void text()
+    {
+
+        if (figures != null)
+        {
+            foreach (var figure in figures)
+            {
+                Console.WriteLine($"Имя: {figure.Name}");   
+            }
+        }
+    }
+}
+
+public enum FileType
+{
+    Txt,
+    Json,
+    Xml,
+    Unknown
 }
